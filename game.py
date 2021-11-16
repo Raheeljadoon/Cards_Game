e1d763d037934b0010ed7059d96987b545d5978c
raheel jadoon <raheeljadoon56@gmail.com>
2021-11-16 16:32:01 +0500

    updating and cleaning

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
-import random 
+import random
 
+
+print(50*'*') 
+print(20*'*', "Game Started",20*"*")
+print(50*'*')
 deck_data = []
 
 shape_dic = {0:"CLUB",1:"SPADE",2:"HEART",3:"DIAMOND"}
@@ -45,7 +49,7 @@ for i in range(0,52):
 random_cards = random.sample(list_range,15)
 
 
-print("before loop lenghth of deck is :",len(deck_data))
+print("Total Cards in deck are  :",len(deck_data))
 
 
 player1_list = [random_cards[0],random_cards[1],random_cards[2],random_cards[3],random_cards[4]]
@@ -53,16 +57,16 @@ player2_list = [random_cards[5],random_cards[6],random_cards[7],random_cards[8],
 player3_list = [random_cards[10],random_cards[11],random_cards[12],random_cards[13],random_cards[14]]
 
 
-print(player1_list)
-print(player2_list)
-print(player3_list)
+# print(player1_list)
+# print(player2_list)
+# print(player3_list)
 
 
 player1_cards = []
 
 for numb1 in player1_list:
     player1_cards.append(deck_data[numb1])
-    print(numb1)
+    # print(numb1)
     
 
 print("**************  Player 1 cards ***************")
@@ -98,7 +102,7 @@ print(player3_cards)
 
 
 combine_list = player1_cards + player2_cards + player3_cards
-print("combine list is --------------------",combine_list)
+# print("combine list is --------------------",combine_list)
 
 deck_after_distribution = []
 num_ = 0
@@ -115,9 +119,11 @@ while num_ < 51:
         
         num_ += 1
         
-
-print("after distribution of cards :",len(deck_after_distribution))
-
+print(50*'*') 
+print(50*'*') 
+print(20*'*' ,"After distribution of cards :",len(deck_after_distribution))
+print(50*'*') 
+print(50*'*') 
 
 
 
